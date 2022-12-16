bcd3d2e9d6f5c128e27420635744965c9df8ac2c
Namit Malasi <namitmalasi@gmail.com>
2022-12-16 19:50:26 +0530

    created the navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const links = [
+    {
+      id: 1,
+      link: "Home",
+    },
+    {
+      id: 2,
+      link: "About",
+    },
+    {
+      id: 3,
+      link: "Portfolio",
+    },
+    {
+      id: 4,
+      link: "Projects",
+    },
+    {
+      id: 5,
+      link: "Contact",
+    },
+  ];
+  return (
+    <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
+      <div>
+        <h1 className="text-5xl font-signature ml-2">Namit</h1>
+      </div>
+
+      <ul className="hidden md:flex">
+        {links.map(({ id, link }) => {
+          return (
+            <li
+              key={id}
+              className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            >
+              {link}
+            </li>
+          );
+        })}
+      </ul>
+
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800 text-gray-500">
+          {links.map(({ id, link }) => {
+            return (
+              <li
+                key={id}
+                className="px-4 py-6 cursor-pointer capitalize text-4xl"
+              >
+                {link}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
