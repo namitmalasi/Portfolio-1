30f1c34764fd6590c6e1fef42fc8e10999770c60
Namit Malasi <namitmalasi@gmail.com>
2022-12-17 16:40:54 +0530

    added social links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Home />
+
+      <SocialLinks />
     </div>
   );
 }
---
+++ src/components/SocialLinks.js
@@ -0,0 +1,75 @@
+import React from "react";
+import { FaGithub, FaLinkedin, FaDev } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+
+const SocialLinks = () => {
+  const links = [
+    {
+      id: 1,
+      child: (
+        <>
+          LinkedIn <FaLinkedin size={30} />
+        </>
+      ),
+      href: "https://www.linkedin.com/in/namit-malasi-a4b56310a/",
+      style: "rounded-tr-md",
+    },
+    {
+      id: 2,
+      child: (
+        <>
+          Github <FaGithub size={30} />
+        </>
+      ),
+      href: "https://github.com/namitmalasi",
+    },
+    {
+      id: 3,
+      child: (
+        <>
+          Dev <FaDev size={30} />
+        </>
+      ),
+      href: "https://dev.to/namitmalasi",
+    },
+    {
+      id: 4,
+      child: (
+        <>
+          Resume <BsFillPersonLinesFill size={30} />
+        </>
+      ),
+      href: "/#",
+      style: "rounded-br-md",
+      //   download: true,
+    },
+  ];
+  return (
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
+      <ul>
+        {links.map(({ id, child, href, style, download }) => (
+          <li
+            key={id}
+            className={
+              "flex justify-between items-center w-40 h-14 px-4 bg-gray-500 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300" +
+              " " +
+              style
+            }
+          >
+            <a
+              href={href}
+              download={download}
+              target="_blank"
+              rel="noreferrer noopener"
+              className="flex justify-between items-center w-full text-white"
+            >
+              {child}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SocialLinks;
